2dcf4fa9cbefd71bff3ef03c05fb2324d1f8f852
zmoitier <z.moitier@gmail.com>
2025-03-23 21:09:31 +0100

    made it worked for orthotope

--- dev/main.jl
+++ dev/main.jl
@@ -17,6 +17,7 @@ using HAdaptiveIntegration:
     integral_monomial_orthotope,
     integral_monomial_simplex
 using LinearAlgebra
+using Optim
 using SparseArrays
 using StaticArrays
 
@@ -45,82 +46,75 @@ function integral_chebyshev_orthotope(d::Int, tdm::Int)
     return indexes
 end
 
-function pack(S::DataType, nodes, weights)
+# [ nodes[1]..., nodes[2]..., ..., nodes[end]..., weights_high..., weights_low... ]
+function pack(S::DataType, ec::EmbeddedCubature{D,T}) where {D,T}
     U = Vector{S}()
-    for (x, w) in zip(nodes, weights)
-        append!(U, S.(x))
-        push!(U, S(w))
+    for node in ec.nodes
+        append!(U, node)
     end
+    append!(U, ec.weights_high)
+    append!(U, ec.weights_low)
     return U
 end
 
-function unpack(U::Vector{T}, D::Int) where {T}
-    nodes, weights = Vector{SVector{D,T}}(), Vector{T}()
-    for t in partition(U, D + 1)
-        push!(nodes, SVector{D}(t[1:D]))
-        push!(weights, t[D + 1])
-    end
-    return nodes, weights
+function unpack(U::Vector{T}, D::Int, H::Int) where {T}
+    nodes = [SVector{D}(t) for t in partition(U[1:(H * D)], D)]
+    weights_high = U[(H * D + 1):(H * D + H)]
+    weights_low = U[(H * D + H + 1):end]
+    return EmbeddedCubature(nodes, weights_high, weights_low)
 end
 
 function increase_precision(
     S::DataType, ec::EmbeddedCubature{D,T}, order_high::Int, order_low::Int
 ) where {D,T}
-    nlo, wlo = increase_precision(
-        S, ec.nodes[1:length(ec.weights_low)], ec.weights_low, order_low
-    )
-    nhi, whi = increase_precision(S, ec.nodes, ec.weights_high, order_high)
-
-    return nlo, wlo, nhi, whi
-end
-
-function increase_precision(S::DataType, nodes, weights, order::Int)
-    D = length(first(nodes))
+    H, L = length(ec.weights_high), length(ec.weights_low)
 
     monomials, values = Vector{NTuple{D,Int}}(), Vector{S}()
-    for pairs in integral_chebyshev_orthotope(D, order)
+    for pairs in integral_chebyshev_orthotope(D, order_high)
         for (multi_index, value) in pairs
             push!(monomials, multi_index)
             push!(values, value)
         end
     end
 
-    U = pack(S, nodes, weights)
-
     function F(u)
-        v = zeros(eltype(u), length(monomials))
-        for (i, mi) in enumerate(monomials)
-            v[i] += sum(
-                prod(cos.(mi .* acos.(2 .* t[1:D] .- 1))) * t[D + 1] for
-                t in partition(u, D + 1)
-            )
+        v = zero(eltype(u))
+
+        nodes = @view u[1:(H * D)]
+        weights_hi = @view u[(H * D + 1):(H * D + H)]
+        weights_lo = @view u[(H * D + H + 1):end]
+
+        acos_nodes = acos.(2 .* nodes .- 1)
+
+        for (mi, val) in zip(monomials[1:order_low], values[1:order_low])
+            v_lo, v_hi = zero(v), zero(v)
+            for (i, acn) in enumerate(partition(acos_nodes, D))
+                tmp = prod(cos.(mi .* acn))
+                v_hi += weights_hi[i] * tmp
+                if i ≤ L
+                    v_lo += weights_lo[i] * tmp
+                end
+            end
+            v += (v_hi - val)^2 + (v_lo - val)^2
         end
-        v -= values
+        for (mi, val) in zip(monomials[(order_low + 1):end], values[(order_low + 1):end])
+            v_hi = zero(v)
+            for (acn, w_hi) in zip(partition(acos_nodes, D), weights_hi)
+                v_hi += w_hi * prod(cos.(mi .* acn))
+            end
+            v += (v_hi - val)^2
+        end
+
         return v
     end
 
-    f, J = F(U), jacobian(F, U)
+    U = pack(S, ec)
+    @show F(U)
 
-    qr_decomp = qr(J)
-    display(qr_decomp.Q)
-    display(sparse(qr_decomp.R))
+    @show result = optimize(F, U, LBFGS(), Optim.Options(; g_tol=1e-35); autodiff=:forward)
+    V = Optim.minimizer(result)
 
-    @show norm(f, Inf)
-    @show size(J)
-    @show cond(J)
-
-    println("start newton")
-    display(maximum(abs.(jacobian(F, U) \ F(U))))
-    for _ in 1:3
-        U -= jacobian(F, U) \ F(U)
-        display(maximum(abs.(jacobian(F, U) \ F(U))))
-    end
-    println("end newton")
-
-    println()
-
-    n, w = unpack(U, D)
-    return n, w
+    return unpack(V, D, length(ec.weights_high))
 end
 
 println("-"^32)
@@ -135,14 +129,13 @@ tec = SEGMENT_GK15
 
 H, L = length(tec.weights_high), length(tec.weights_low)
 ec = embedded_cubature(Float64, tec)
-nlo, wlo, nhi, whi = increase_precision(
-    Float64, embedded_cubature(Float32, tec), tec.order_high, tec.order_low
+ec_ex = increase_precision(
+    BigFloat, embedded_cubature(Float64, tec), tec.order_high, tec.order_low
 )
 
+display(norm(norm.(ec_ex.nodes - ec.nodes, Inf), Inf))
 println("Low order")
-display(norm(norm.(nlo - ec.nodes[1:L], Inf), Inf))
-display(norm(wlo - ec.weights_low, Inf))
+display(norm(ec_ex.weights_low - ec.weights_low, Inf))
 println()
 println("High order")
-display(norm(norm.(nhi - ec.nodes, Inf), Inf))
-display(norm(whi - ec.weights_high, Inf))
+display(norm(ec_ex.weights_high - ec.weights_high, Inf))
