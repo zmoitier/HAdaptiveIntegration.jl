df7c19ec7c5f640bd3a2edc9737d5071a3718748
Zoïs Moitier <zois.moitier@ensta-paris.fr>
2025-04-04 11:39:32 +0200

    rework test integrate

--- test/test_integrate.jl
+++ test/test_integrate.jl
@@ -34,128 +34,95 @@ end
 end
 
 @testset "Integrate over a segment" begin
-    ec = hai.embedded_cubature(Float64, hai.SEGMENT_GK15)
     segment = hai.segment(0, 1)
+    buffer = hai.allocate_buffer(x -> zero(x[1]), segment)
 
-    I, E = ec(x -> exp(x[1]), segment)
-    R = exp(1) - exp(0)
-    @test abs(I - R) ≤ E * abs(R)
-
-    I, E = ec(x -> cos(10 * x[1]), segment)
-    R = sin(10) / 10
-    @test abs(I - R) ≤ E * abs(R)
-
-    I, E = ec(x -> 1 / √x[1], segment)
-    R = 2
-    @test abs(I - R) ≤ E * abs(R)
-
-    I, E = hai.integrate(x -> 1 / √x[1], segment)
-    R = 2
-    @test abs(I - R) ≤ E * abs(R)
-end
-
-@testset "Integrate over a rectangle" begin
-    ec = hai.embedded_cubature(Float64, hai.SQUARE_CH25)
-    square = hai.rectangle((0, 0), (1, 1))
-
-    I, E = ec(x -> exp(x[1] + x[2]), square)
-    R = (exp(1) - exp(0))^2
-    @test abs(I - R) ≤ E * abs(R)
-
-    I, E = ec(x -> 1 / norm(x), square)
-    R = 1 / 2 * log(17 + 12 * sqrt(2))
-    @test abs(I - R) ≤ E * abs(R)
-
-    I, E = hai.integrate(x -> 1 / norm(x), square)
-    @test abs(I - R) < 1e-8
-    @test abs(I - R) ≤ E * abs(R)
+    for (fct, R) in [
+        (x -> exp(x[1]), exp(1) - 1),
+        (x -> cos(10 * x[1]), sin(10) / 10),
+        (x -> 1 / √x[1], 2),
+    ]
+        I, E = hai.integrate(fct, segment; buffer=buffer)
+        @test abs(I - R) ≤ E * abs(R)
+    end
 end
 
 @testset "Integrate over a triangle" begin
-    ec = hai.embedded_cubature(Float64, hai.TRIANGLE_RL19)
-
     triangle = hai.triangle((0, 0), (2, 0), (0, 2))
-    I, E = ec(x -> exp(x[1] + 3 * x[2]), triangle)
-    R = (exp(6) - 3 * exp(2) + 2) / 6
-    @test abs(I - R) ≤ E * abs(R)
+    buffer = hai.allocate_buffer(x -> zero(x[1]), triangle)
 
-    I, E = ec(x -> cos(7 * x[1] + 3 * x[2]), triangle)
-    R = (-3 * cos(14) + 7 * cos(6) - 4) / 84
-    @test abs(I - R) ≤ E * abs(R)
+    for ec in
+        (hai.default_embedded_cubature(triangle), hai.embedded_cubature(hai.TRIANGLE_GM19))
+        for (fct, R) in [
+            (x -> exp(x[1] + 3 * x[2]), (exp(6) - 3 * exp(2) + 2) / 6),
+            (x -> cos(7 * x[1] + 3 * x[2]), (-3 * cos(14) + 7 * cos(6) - 4) / 84),
+            (x -> 1 / norm(x), 2 * sqrt(2) * asinh(1)),
+        ]
+            I, E = hai.integrate(fct, triangle; embedded_cubature=ec, buffer=buffer)
+            @test abs(I - R) ≤ E * abs(R)
+        end
+    end
+end
 
-    triangle = hai.triangle((0, 0), (1, 0), (0, 1))
-    I, E = hai.integrate(x -> 1 / norm(x), triangle)
-    R = sqrt(2) * asinh(1)
-    @test abs(I - R) ≤ E * abs(R)
+@testset "Integrate over a rectangle" begin
+    square = hai.rectangle((0, 0), (1, 1))
+    buffer = hai.allocate_buffer(x -> zero(x[1]), square)
+
+    for ec in (
+        hai.default_embedded_cubature(square),
+        hai.embedded_cubature(hai.SQUARE_CH21),
+        hai.embedded_cubature(hai.SQUARE_GM17),
+    )
+        for (fct, R) in [
+            (x -> exp(x[1] + x[2]), (exp(1) - 1)^2),
+            (x -> 1 / norm(x), log(17 + 12 * sqrt(2)) / 2),
+        ]
+            I, E = hai.integrate(fct, square; embedded_cubature=ec, buffer=buffer)
+            @test abs(I - R) ≤ E * abs(R)
+        end
+    end
+end
+
+@testset "Integrate over a tetrahedron" begin
+    triangle = hai.tetrahedron((0, 0, 0), (1, 0, 0), (0, 1, 0), (0, 0, 1))
+    buffer = hai.allocate_buffer(x -> zero(x[1]), triangle)
+
+    for (fct, R) in [
+        (
+            x -> exp(x[1] + 3 * x[2] + 5 * x[3]),
+            (3 * exp(5) - 10 * exp(3) + 15 * exp(1) - 8) / 120,
+        ),
+        (x -> 1 / norm(x), 0.3614258523411),
+    ]
+        I, E = hai.integrate(fct, triangle; buffer=buffer)
+        @test abs(I - R) ≤ E * abs(R)
+    end
 end
 
 @testset "Integrate over a Cuboid" begin
-    ec = hai.embedded_cubature(Float64, hai.CUBE_BE65)
     cube = hai.cuboid((0, 0, 0), (1, 1, 1))
+    buffer = hai.allocate_buffer(x -> zero(x[1]), cube)
 
-    I, E = ec(x -> exp(x[1]), cube)
-    R = exp(1) - exp(0)
-    @test abs(I - R) < 1e-8
-    @test abs(I - R) ≤ E * abs(R)
-
-    I, E = ec(x -> exp(x[1] + x[2] + x[3]), cube)
-    R = (exp(1) - exp(0))^3
-    @test abs(I - R) ≤ E * abs(R)
-
-    I, E = ec(x -> 1 / (1 + norm(x)^2)^2, cube)
-    R = π^2 / 32
-    @test abs(I - R) ≤ E * abs(R)
-
-    I, E = hai.integrate(x -> 1 / norm(x), cube)
-    @test E ≤ √eps(Float64) * I
+    for ec in (hai.default_embedded_cubature(cube), hai.embedded_cubature(hai.CUBE_GM33))
+        for (fct, R) in [
+            (x -> exp(x[1]), exp(1) - 1),
+            (x -> 1 / (1 + norm(x)^2)^2, π^2 / 32),
+            (x -> 1 / norm(x), 1.1900386819897766),
+        ]
+            I, E = hai.integrate(fct, cube; embedded_cubature=ec, buffer=buffer)
+            @test abs(I - R) ≤ E * abs(R)
+        end
+    end
 end
 
-@testset "GrundmannMoeller quadrature" begin
-    @testset "Triangle" begin
-        ec = hai.embedded_cubature(Float64, hai.TRIANGLE_GM19)
-        rtol = 1e-8
+@testset "4-Simplex" begin
+    I, E = hai.integrate(x -> 1 / norm(x), hai.reference_simplex(4))
+    R = 0.089876019011
+    @test abs(I - R) ≤ E * abs(R)
+end
 
-        triangle = hai.triangle((0, 0), (2, 0), (0, 2))
-        I, E = hai.integrate(
-            x -> exp(x[1] + 3 * x[2]), triangle; embedded_cubature=ec, rtol=rtol
-        )
-        R = (exp(6) - 3 * exp(2) + 2) / 6
-        @test abs(I - R) ≤ rtol * abs(R)
-
-        I, E = hai.integrate(
-            x -> cos(7 * x[1] + 3 * x[2]), triangle; embedded_cubature=ec, rtol=rtol
-        )
-        R = (-3 * cos(14) + 7 * cos(6) - 4) / 84
-        @test abs(I - R) ≤ rtol * abs(R)
-
-        triangle = hai.triangle((0, 0), (1, 0), (0, 1))
-        I, E = hai.integrate(x -> 1 / norm(x), triangle; embedded_cubature=ec, rtol=rtol)
-        R = sqrt(2) * asinh(1)
-        @test abs(I - R) ≤ rtol * abs(R)
-    end
-
-    @testset "Tetrahedron" begin
-        tetrahedron = hai.tetrahedron((0, 0, 0), (1, 0, 0), (0, 1, 0), (0, 0, 1))
-        ec = hai.embedded_cubature(Float64, hai.TETRAHEDRON_GM35)
-        rtol = 1e-8
-
-        I, E = hai.integrate(x -> 1, tetrahedron; rtol=rtol)
-        R = 1 / 6
-        @test abs(I - R) ≤ rtol * abs(R)
-
-        I, E = hai.integrate(x -> exp(x[1] + 3 * x[2] + 5 * x[3]), tetrahedron; rtol=rtol)
-        R = (3 * exp(5) - 10 * exp(3) + 15 * exp(1) - 8) / 120
-        @test abs(I - R) ≤ rtol * abs(R)
-
-        I, E = hai.integrate(x -> 1 / norm(x), tetrahedron; rtol=rtol)
-        R = 0.3614258523411 # reference computed using BigFloat
-        @test abs(I - R) ≤ rtol * abs(R)
-    end
-
-    @testset "4-Simplex" begin
-        rtol = 1e-8
-        I, E = hai.integrate(x -> 1 / norm(x), hai.reference_simplex(4); rtol=rtol)
-        R = 0.089876019011 # reference computed using BigFloat
-        @test abs(I - R) ≤ rtol * abs(R)
-    end
+@testset "4-Orthotope" begin
+    I, E = hai.integrate(x -> 1 / norm(x), hai.reference_orthotope(4))
+    R = 0.9674120212411487
+    @test abs(I - R) ≤ E * abs(R)
 end
